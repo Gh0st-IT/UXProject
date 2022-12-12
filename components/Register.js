abd99e9dae108fb17b7ccabc08a17d417b5f6037
Gh0st-IT <ghostraegan16@gmail.com>
2022-12-12 11:08:07 +0800

    Update Register.js

--- components/Register.js
+++ components/Register.js
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 const Register = ()=>{
     return(
         <View>
-            <Text>Register View</Text>
+            <Text>Register Component View</Text>
         </View>
     )
 }
